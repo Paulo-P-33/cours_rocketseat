01a98625f7e5a371f050b9a357aa0f66bc56d0bf
Paulo-P-33 <trab0291997@gmail.com>
2022-08-26 18:01:25 -0300

    feat: add lib styled-components

---
+++ React-native/myfinances/App.tsx
@@ -0,0 +1,10 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Dashboard } from './src/screens/Dashboard';
+
+export default function App() {
+  return <Dashboard/>;
+}
+
+
---
+++ React-native/myfinances/src/screens/Dashboard/styles.ts
@@ -0,0 +1,13 @@
+import Styled from 'styled-components/native';
+
+export const Container = Styled.View`
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const Title = Styled.Text`
+    color: purple;
+    font-size: 30px;
+    font-weight: bold;
+`;
