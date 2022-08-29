f4ccbd213cffffd9bd0baa3de0121cd73d7988fe
Paulo-P-33 <trab0291997@gmail.com>
2022-08-29 17:32:28 -0300

    feat: add proportion and meansure

--- React-native/myfinances/src/screens/Dashboard/index.tsx
+++ React-native/myfinances/src/screens/Dashboard/index.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
-import { Container, Title } from './styles';
+import { Container, Header, UserWrapper, UserName, UserInfo, User, UserGreeting, Photo} from './styles';
 
 
 export function Dashboard() {
     return (
         <Container>
-            <Title>Dashboard</Title>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo source={{ uri:'https://avatars.githubusercontent.com/u/74207343?v=4'} }/>
+                        <User>
+                            <UserGreeting>Olá, </UserGreeting>
+                            <UserName>Luiz</UserName>
+                        </User>
+                    </UserInfo>
+                </UserWrapper>
+            </Header>
         </Container>
     )
 }
--- React-native/myfinances/src/screens/Dashboard/styles.ts
+++ React-native/myfinances/src/screens/Dashboard/styles.ts
@@ -1,14 +1,52 @@
 import Styled from 'styled-components/native';
+import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
 
 export const Container = Styled.View`
     flex: 1;
-    align-items: center;
-    justify-content: center;
     background-color: ${({theme}) => theme.colors.background};
 `;
 
-export const Title = Styled.Text`
+export const Header = Styled.View`
+    width: 100%;
+    height: ${RFPercentage(42)}px;
+    background-color: ${({theme}) => theme.colors.primary};
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+`;
+
+export const UserWrapper = Styled.View`
+    width: 100%;
+
+    padding: 0 24px;
+`;
+
+export const UserInfo = Styled.View`
+    flex-direction: row;
+    align-items: center;
+`;
+
+export const Photo = Styled.Image`
+    width: ${RFValue(48)}px;
+    height: ${RFValue(48)}px;
+
+    border-radius: 10px;
+`;
+
+export const User = Styled.View`
+    margin-left: 17px;
+`;
+
+export const UserGreeting = Styled.Text`
+    color: ${({theme}) => theme.colors.shape};
+
+    font-size: ${RFValue(18)}px;
+    font-family: ${({theme}) => theme.fonts.regular};
+`;
+
+export const UserName = Styled.Text`
+    color: ${({theme}) => theme.colors.shape};
+
+    font-size: ${RFValue(18)}px;
     font-family: ${({theme}) => theme.fonts.bold};
-    color: ${({theme}) => theme.colors.title} ;
-    font-size: 30px;
 `;
